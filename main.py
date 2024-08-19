8f0077994cf1dc70d721c220a85d9a7e132c2ca3
bones616 <mr.dobriyzhuk@gmail.com>
2024-08-19 23:47:38 +0300

    add file

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+
+def get_joke() :
+    url = 'https://api.chucknorris.io/jokes/random'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+       joke = response.json()['value']
+    else:
+        joke = 'No jokes'
+    return joke
+
+def len_joke() :
+    joke = get_joke()
+    return len(joke)
+
+if __name__== '__main__':
+    print(get_joke())
+
+
---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest.mock import patch
+from unittest.mock import MagicMock
+from unittest.mock import self
+
+
+from main import get_joke, len_joke
+
+
+class TestJoke(unittest.TestCase):
+
+    @patch('main.get_joke')
+    def test_len_joke(self, mock_get_joke):
+        mock_get_joke.return_value = 'one'
+        self.assertEqual(len_joke(), 3)
+
+    @patch('main.requests')
+    def test_get_joke(self, mock_requests):
+       mock_response = MagicMock()
+       mock_response.status_code = 200
+          'value' : 'Hello world'
+       }
+       mock_requests.get.return_value = mock_response
+       self.assertEqual(get_joke(), 'Hello world')
+
+    @patch('main.requests')
+    def test_fail_get_joke(self, mock_requests):
+        mock_response = MagicMock(status_code=403)
+        mock_requests.get.return_value = mock_response
+        self.assertEqual(get_joke(), 'No jokes')
+
+
+    if __name__ == '__main__':
+        unittest.main()
